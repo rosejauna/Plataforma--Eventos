3011c595cfac16b1bd176f61681facf66ec0e9bd
Emerson de Souza <emersonsdom13@hotmail.com>
2025-12-14 03:21:40 -0300

    eventDAO

--- src/dao/eventDAO.js
+++ src/dao/eventDAO.js
@@ -3,7 +3,7 @@ import api from "../api/api";
 export default {
   async findAll() {
     const res = await api.get("/eventos");
-    return res.data || [];
+    return res.data;
   },
 
   async findById(id) {
@@ -24,5 +24,15 @@ export default {
   async delete(id) {
     const res = await api.delete(`/eventos/${id}`);
     return res.data;
-  }
+  },
+
+  async addParticipant(eventId, participantId) {
+    const res = await api.post(`/eventos/${eventId}/participantes`, { participantId });
+    return res.data;
+  },
+
+  async removeParticipant(eventId, participantId) {
+    const res = await api.delete(`/eventos/${eventId}/participantes/${participantId}`);
+    return res.data;
+  },
 };
